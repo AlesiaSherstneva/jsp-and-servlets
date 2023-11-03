631c15e4ed40ed5f6f102a0cc649144080f66791
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-11-03 14:37:42 +0300

    Create class for student POJO

---
+++ 02-tagdemo/src/main/java/com/luv2code/jsp/Student.java
@@ -0,0 +1,14 @@
+package com.luv2code.jsp;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Student {
+    private String firstName;
+    private String lastName;
+    private boolean goldCustomer;
+}
