97eb1ca24e4c3d49da9c35928562172f2833ad99
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-11-10 01:03:39 +0300

    Receive some params from web.xml

--- 03-servletdemo/src/main/java/com/luv2code/servlets/TestParamServlet.java
+++ 03-servletdemo/src/main/java/com/luv2code/servlets/TestParamServlet.java
@@ -1,20 +1,34 @@
 package com.luv2code.servlets;
 
-import jakarta.servlet.*;
-import jakarta.servlet.http.*;
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.annotation.*;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "TestParamServlet", value = "/TestParamServlet")
 public class TestParamServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
 
+        ServletContext context = getServletContext();
+        String maxCardSize = context.getInitParameter("max-shopping-cart-size");
+        String teamName = context.getInitParameter("project-team-name");
+
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("Max cart: " + maxCardSize);
+        out.println("<br/><br/>");
+        out.println("Team name: " + teamName);
+        out.println("</body></html>");
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        doGet(request, response);
     }
 }
