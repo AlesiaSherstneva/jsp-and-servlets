82de5719f2df2a498de93621f284e52a0a7367f0
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-11-12 13:33:47 +0300

    Update MVC servlet for retrieving students from the util class

--- 03-servletdemo/src/main/java/com/luv2code/servlets/MvcDemoServlet.java
+++ 03-servletdemo/src/main/java/com/luv2code/servlets/MvcDemoServlet.java
@@ -1,16 +1,19 @@
 package com.luv2code.servlets;
 
+import com.luv2code.servlets.pojo.Student;
+import com.luv2code.servlets.util.StudentDataUtil;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "MvcDemoServlet", value = "/MvcDemoServlet")
 public class MvcDemoServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String[] students = {"Susan", "Anil", "Mohamed", "Trupti"};
+        List<Student> students = StudentDataUtil.getStudents();
         request.setAttribute("student_list", students);
 
         RequestDispatcher dispatcher = request.getRequestDispatcher("/view-students.jsp");
