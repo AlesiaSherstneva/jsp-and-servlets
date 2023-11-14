d223d4fd7714f40a5766d29b53a13e1a8db53a89
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-11-15 01:51:42 +0300

    Retrieve the data from DB

--- 04-web-student-tracker/src/main/java/com/luv2code/web/TestServlet.java
+++ 04-web-student-tracker/src/main/java/com/luv2code/web/TestServlet.java
@@ -1,16 +1,45 @@
 package com.luv2code.web;
 
-import jakarta.servlet.*;
-import jakarta.servlet.http.*;
+import jakarta.annotation.Resource;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.*;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
+import javax.sql.DataSource;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 @WebServlet(name = "TestServlet", value = "/TestServlet")
 public class TestServlet extends HttpServlet {
+    @Resource(name="jdbc/web_student_tracker")
+    private DataSource dataSource;
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
 
+        Connection connection;
+        Statement statement;
+        ResultSet resultSet;
+
+        try {
+            connection = dataSource.getConnection();
+            String sql = "SELECT * FROM student;";
+            statement = connection.createStatement();
+
+            resultSet = statement.executeQuery(sql);
+            while (resultSet.next()) {
+                String email = resultSet.getString("email");
+                out.println(email);
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
     }
 
     @Override
