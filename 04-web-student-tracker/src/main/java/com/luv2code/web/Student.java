235db265b0c48a418d63bd31e228ba52776cad1a
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-11-18 14:54:49 +0300

    Create POJO class for student

---
+++ 04-web-student-tracker/src/main/java/com/luv2code/web/Student.java
@@ -0,0 +1,23 @@
+package com.luv2code.web;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+public class Student {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public Student(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+}
